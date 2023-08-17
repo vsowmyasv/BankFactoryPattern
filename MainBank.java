3cc6ec0377390846dc8797fbcae30463b01fd93b
vsowmyasv <vsowmyasv@gmail.com>
2023-08-17 12:20:05 +0530

    -

--- MainBank.java
+++ MainBank.java
@@ -5,6 +5,24 @@ public class MainBank {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		CurrentAccountFunctionalities ca = new CurrentAccountFunctionalities();
+		
+		ca.createAccount();
+		ca.calculateInterest();
+		ca.showBalance();
+		
+		LoanFunctionalities la = new LoanFunctionalities();
+		
+		la.createAccount();
+		la.calculateInterest();
+		la.calculateLoanPayment(40000, 2);
+		
+		SavingsAccountFunctionalities sa = new SavingsAccountFunctionalities();
+		
+		sa.createAccount();
+		sa.calculateInterest();
+		sa.showBalance();
+		
 	}
 
 }
